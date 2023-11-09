e64934d067a68091590496c05ac1f55fbba372da
Sayantan <sayantan0504@gmail.com>
2023-11-10 03:56:00 +0530

    Display Chat History✅

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
 from html_templates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -17,65 +18,87 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
+
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000, 
+        chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
     return chunks
 
-# Since OpenAIEmbeddings cost money we can use HuggingFaceInstructEmbeddings to get embeddings in our computer
-def get_embeddings(text_chunks):
+
+def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vector_store = FAISS.from_texts(texts=text_chunks, embeddings=embeddings)
-    return vector_store
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
-def get_conversation_chain(vector_store):
+
+def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        retriever=vector_store.as_retriever(),
+        retriever=vectorstore.as_retriever(),
         memory=memory
     )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Mutli-Chat PDF", page_icon=":books:")
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
-    # Persists the conversation state variable across re-renders
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents: ")
-    
-
-    st.write(user_template.replace("{{MSG}}", "Hello App!"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Hello Human!"), unsafe_allow_html=True)
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
-        st.subheader("Your PDFs")
-        pdf_docs = st.file_uploader("Upload your PDFs here", type="pdf", accept_multiple_files=True)
-        if st.button("Upload"):
-            with st.spinner("Uploading PDFs..."):
-                #1. Extract the text from the PDF
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                
-                #2. Get the text chunks from the PDF
+
+                # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                
-                #3. Create vector store or embeddings
-                vector_store = get_embeddings(text_chunks)
 
-                #4. Create conversation chain
-                st.session_state.conversation = get_conversation_chain(vector_store) # Making session state persistent because streamlit reinitializes the environment
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
-if __name__ == "__main__":
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
     main()
